6485a8b04d72d93aead01f1aaa388de1f03ff8b9
pdierssen <dierssen.philipp@outlook.de>
2024-07-27 17:57:35 +0200

    bufferedWriter

---
+++ src/control/FileOutput.java
@@ -0,0 +1,59 @@
+package control;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileOutput implements IFileMerger
+{
+    private File result;
+    private FileWriter fileWriter;
+    private BufferedWriter bufferedWriter;
+
+    public FileOutput (String fileName)
+    {
+        result = new File(fileName);
+        try
+        {
+            fileWriter = new FileWriter(result);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (IOException e)
+        {
+            System.out.println("err" + e.getMessage());
+        }
+    }
+
+    @Override public void mergeFiles(List<Path> list)
+    {
+        list.forEach(path ->
+        {
+            try {
+                FileReader fileReader = new FileReader(path.toFile());
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line = bufferedReader.readLine();
+                while (line != null)
+                {
+                    System.out.println(line);
+                    bufferedWriter.write(line);
+                    bufferedWriter.newLine();
+                    line = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+            } catch (FileNotFoundException e)
+            {
+                System.out.println("Error" + e.getMessage());
+            } catch (IOException e)
+            {
+                System.out.println("IOErrror" + e.getMessage());
+            }
+        });
+        try
+        {
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e)
+        {
+            System.out.println("probelm with closing" + e.getMessage());
+        }
+    }
+}
--- src/control/Main.java
+++ src/control/Main.java
@@ -1,12 +1,7 @@
 package control;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Main
@@ -14,7 +9,10 @@ public class Main
     public static void main(String[] args)
     {
         Directory dir = new Directory("src/resources/directoy");
+        File result = new File("src/resources/result12.txt");
         List<Path> paths = dir.extractFilePaths();
         System.out.println(paths);
+        FileOutput fileOutput = new FileOutput("src/resources/result187.txt");
+        fileOutput.mergeFiles(paths);
     }
 }
